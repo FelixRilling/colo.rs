aa2e9c53e0b417c452be41d1a8343d9d916acdde
FelixRilling <contact@rilling.dev>
2021-05-08 18:46:33 +0200

    Started adding tests.

--- src/core/contrast.rs
+++ src/core/contrast.rs
@@ -1,10 +1,9 @@
-use crate::core::color::{RGB};
-
+use crate::core::color::RGB;
 
 /// Calculates the WCAG color ratio of two colors.
 /// The same color inputs will produce the same output regardless of position.
-// https://css-tricks.com/understanding-web-accessibility-color-contrast-guidelines-and-ratios/
 // https://www.w3.org/TR/WCAG20-TECHS/G18.html#G18-tests
+// https://css-tricks.com/understanding-web-accessibility-color-contrast-guidelines-and-ratios/
 // https://github.com/tmcw/wcag-contrast/blob/master/index.js
 pub fn contrast_ratio(color_1: &RGB, color_2: &RGB) -> f32 {
     let color_1_luminance = relative_luminance(color_1);
@@ -35,5 +34,46 @@ fn transform_color_value(rgb_val: u8) -> f32 {
         adapted_val / 12.92
     } else {
         ((adapted_val + 0.055) / 1.055).powf(2.4)
+    }
+}
+
+// Tests based on https://github.com/tmcw/wcag-contrastresults
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use float_cmp::approx_eq;
+
+    use crate::core::color::RGB;
+
+    use super::contrast_ratio;
+
+    #[test]
+    fn contrast_ratio_same_color() {
+        let black = RGB::from_str("#000000").unwrap();
+
+        let expected: f32 = 1.0;
+        let actual = contrast_ratio(&black, &black);
+        assert!(approx_eq!(f32, actual, expected, ulps = 2));
+    }
+
+    #[test]
+    fn contrast_ratio_max_contrast() {
+        let black = RGB::from_str("#000000").unwrap();
+        let white = RGB::from_str("#FFFFFF").unwrap();
+
+        let expected: f32 = 21.0;
+        let actual = contrast_ratio(&black, &white);
+        assert!(approx_eq!(f32, actual, expected, ulps = 2));
+    }
+
+    #[test]
+    fn contrast_ratio_ignores_order() {
+        let a = RGB::from_str("#90B5AC").unwrap();
+        let b = RGB::from_str("#662270").unwrap();
+
+        let actual_1 = contrast_ratio(&a, &b);
+        let actual_2 = contrast_ratio(&b, &a);
+        assert_eq!(actual_1, actual_2)
     }
 }
