f10e7fde1daef6924958ad6d16e5c12a7d9a8c07
FelixRilling <contact@rilling.dev>
2021-05-15 19:59:28 +0200

    Split hex parsing/formatting from traits.

--- src/color.rs
+++ src/color.rs
@@ -9,25 +9,32 @@ pub struct RGB {
     pub b: u8,
 }
 
-// https://rust-lang-nursery.github.io/rust-cookbook/text/string_parsing.html
+impl RGB {
+    pub fn from_hex_str(hex_str: &str) -> Result<RGB, std::num::ParseIntError> {
+        // https://rust-lang-nursery.github.io/rust-cookbook/text/string_parsing.html
+        let r: u8 = u8::from_str_radix(&hex_str[1..3], 16)?;
+        let g: u8 = u8::from_str_radix(&hex_str[3..5], 16)?;
+        let b: u8 = u8::from_str_radix(&hex_str[5..7], 16)?;
+
+        Ok(RGB { r, g, b })
+    }
+
+    pub fn to_hex_str(&self) -> String {
+        format!("#{:X}{:X}{:X}", self.r, self.g, self.b)
+    }
+}
+
 impl FromStr for RGB {
     type Err = std::num::ParseIntError;
 
     // Parses a color hex code of the form '#rRgGbB..'
-    fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
-
-        // u8::from_str_radix(src: &str, radix: u32) converts a string
-        // slice in a given base to u8
-        let r: u8 = u8::from_str_radix(&hex_code[1..3], 16)?;
-        let g: u8 = u8::from_str_radix(&hex_code[3..5], 16)?;
-        let b: u8 = u8::from_str_radix(&hex_code[5..7], 16)?;
-
-        Ok(RGB { r, g, b })
+    fn from_str(hex_str: &str) -> Result<Self, Self::Err> {
+        RGB::from_hex_str(hex_str)
     }
 }
 
 impl Display for RGB {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("#{:X}{:X}{:X}", self.r, self.g, self.b))
+        f.write_str(&self.to_hex_str())
     }
 }
