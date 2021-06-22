790326eddd4459308f70b14a3582a97274fac329
FelixRilling <contact@rilling.dev>
2021-06-22 21:44:26 +0200

    Further improve CLI error handling.

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,7 +1,10 @@
 use std::collections::HashSet;
+use std::fmt::Display;
 use std::io::Write;
+use std::ops::Deref;
 
 use clap::{App, Arg, SubCommand};
+use log::{debug, error, info};
 use rug::Float;
 use termcolor::{ColorChoice, StandardStream};
 
@@ -15,14 +18,30 @@ mod color_printing;
 
 const COLOR_ARG_HELP: &str = "CSS-like color value, e.g. #00FF11 or 'rgb(255 128 0)'.";
 
-struct Options {
-    verbosity: u8,
-}
-
-fn parse_color(slice: &str) -> Result<Rgb, ParsingError> {
+fn parse_color(seq: &str) -> Result<Rgb, ParsingError> {
     // TODO: Allow specifying which format should be used instead of trying all.
-    Rgb::from_hex_str(slice)
-        .or_else(|_| Rgb::from_rgb_function_str(slice))
+    debug!("Attempting to parse '{}' as hex string color.", seq);
+    match Rgb::from_hex_str(seq) {
+        Ok(color) => {
+            info!("Successfully parsed '{}' as hex string color: '{}'.", seq, color);
+            Ok(color)
+        }
+        Err(hex_err) => {
+            info!("Could not parse '{}' as hex string color: {}.", seq, hex_err);
+
+            debug!("Attempting to parse '{}' as RGB function string color.", seq);
+            match Rgb::from_rgb_function_str(seq) {
+                Ok(color) => {
+                    info!("Successfully parsed '{}' as RGB function string color: '{}'.", seq, color);
+                    Ok(color)
+                }
+                Err(rgb_function_err) => {
+                    info!("Could not parse '{}' as RGB function string color: {}.", seq, rgb_function_err);
+                    Err(rgb_function_err)
+                }
+            }
+        }
+    }
 }
 
 fn main() {
@@ -31,7 +50,7 @@ fn main() {
             Arg::with_name("v")
                 .short("v")
                 .multiple(true)
-                .help("Sets the level of verbosity.")
+                .help("Increase message verbosity.")
         )
         .subcommand(
             SubCommand::with_name("contrast")
@@ -49,20 +68,23 @@ fn main() {
         )
         .get_matches();
 
-    let options = Options { verbosity: matches.occurrences_of("v") as u8 };
+    let verbosity = matches.occurrences_of("v") as usize;
+    stderrlog::new().verbosity(verbosity).init().expect("Could not initialize logger.");
+
     match matches.subcommand_matches("contrast") {
         Some(matches) => {
             let color_1_str = matches.value_of("color_1").unwrap();
             let color_2_str = matches.value_of("color_2").unwrap();
+
             match parse_color(color_1_str) {
-                Err(e) => eprintln!("Color 1: {}", e),
+                Err(_) => error!("Could not parse color 1."),
                 Ok(color_1) => match parse_color(color_2_str) {
-                    Err(e) => eprintln!("Color 2: {}", e),
-                    Ok(color_2) => print_contrast(&color_1, &color_2, &options).expect("Could not print contrast.")
+                    Err(_) => error!("Could not parse color 2."),
+                    Ok(color_2) => print_contrast(&color_1, &color_2, verbosity).expect("Could not print contrast.")
                 },
             }
         }
-        None => eprintln!("No subcommand provided. See --help.")
+        None => error!("No subcommand provided. See --help.")
     }
 }
 
@@ -78,7 +100,7 @@ fn hash_set_as_sorted_vec<T: Ord>(hash_set: HashSet<T>) -> Vec<T> {
     set_copy_vec
 }
 
-fn print_contrast(color_1: &Rgb, color_2: &Rgb, options: &Options) -> std::io::Result<()> {
+fn print_contrast(color_1: &Rgb, color_2: &Rgb, verbosity: usize) -> std::io::Result<()> {
     let contrast_ratio_val = contrast_ratio_val(color_1, color_2);
     let contrast_levels_reached = contrast_ratio_levels_reached(color_1, color_2);
 
@@ -89,15 +111,14 @@ fn print_contrast(color_1: &Rgb, color_2: &Rgb, options: &Options) -> std::io::R
     write!(&mut stdout, " to ")?;
     print_color(&mut stdout, color_2)?;
 
-    match options.verbosity {
-        verbosity if verbosity >= 1 => writeln!(&mut stdout, " is {}.", contrast_ratio_val)?,
-        _ => {
-            // Usually only displaying the last 2 digits is enough.
-            // Note that we cannot use the rounding provided by the formatter as contrast values may not be rounded up.
-            let floored = floor_n_decimals(&contrast_ratio_val, 2);
-            writeln!(&mut stdout, " is {:.2}.", floored.to_f32())?
-        }
+    let printable_contrast_ratio_val: Box<dyn Display> = if verbosity == 0 {
+        // Usually only displaying the last 2 digits is enough.
+        // Note that we cannot use the rounding provided by the formatter as contrast values may not be rounded up.
+        Box::new(floor_n_decimals(&contrast_ratio_val, 2).to_f32())
+    } else {
+        Box::new(contrast_ratio_val)
     };
+    writeln!(&mut stdout, " is {}.", printable_contrast_ratio_val.deref())?;
 
     let contrast_levels_reached_string: String = if contrast_levels_reached.is_empty() {
         String::from("None")
