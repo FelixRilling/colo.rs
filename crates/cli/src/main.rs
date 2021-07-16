8f0f6cc021d21c7b3c0ecb11a85751990dc38875
FelixRilling <contact@rilling.dev>
2021-07-16 22:46:54 +0200

    Use clap argument validation for colors.

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,5 +1,5 @@
 use clap::{App, Arg, SubCommand};
-use log::{debug, error, info, LevelFilter};
+use log::{debug, info, LevelFilter};
 
 use color_utils::error::ParsingError;
 use color_utils::rgb::Rgb;
@@ -7,32 +7,61 @@ use color_utils::rgb::Rgb;
 mod color_printing;
 mod contrast;
 
-const COLOR_ARG_HELP: &str = "CSS-like color value, e.g. #00FF11 or 'rgb(255 128 0)'.";
+// TODO: Allow specifying which format should be used instead of trying all.
 
-fn parse_color(seq: &str) -> Result<Rgb, ParsingError> {
-    // TODO: Allow specifying which format should be used instead of trying all.
+pub fn is_color(val: String) -> Result<(), String> {
+    if Rgb::from_hex_str(&val).is_ok() || Rgb::from_rgb_function_str(&val).is_ok() {
+        return Ok(());
+    }
+    Err(String::from(
+        "Could not parse the value as a CSS-like color value.",
+    ))
+}
+
+pub fn parse_color(seq: &str) -> Result<Rgb, ParsingError> {
     debug!("Attempting to parse '{}' as hex string color.", seq);
     match Rgb::from_hex_str(seq) {
         Ok(color) => {
-            info!("Successfully parsed '{}' as hex string color: '{}'.", seq, &color);
+            info!(
+                "Successfully parsed '{}' as hex string color: '{}'.",
+                seq, &color
+            );
             Ok(color)
         }
         Err(hex_err) => {
-            info!("Could not parse '{}' as hex string color: {}.", seq, &hex_err);
+            info!(
+                "Could not parse '{}' as hex string color: {}.",
+                seq, &hex_err
+            );
 
-            debug!("Attempting to parse '{}' as RGB function string color.", seq);
+            debug!(
+                "Attempting to parse '{}' as RGB function string color.",
+                seq
+            );
             match Rgb::from_rgb_function_str(seq) {
                 Ok(color) => {
-                    info!("Successfully parsed '{}' as RGB function string color: '{}'.", seq, &color);
+                    info!(
+                        "Successfully parsed '{}' as RGB function string color: '{}'.",
+                        seq, &color
+                    );
                     Ok(color)
                 }
                 Err(rgb_function_err) => {
-                    info!("Could not parse '{}' as RGB function string color: {}.", seq, &rgb_function_err);
+                    info!(
+                        "Could not parse '{}' as RGB function string color: {}.",
+                        seq, &rgb_function_err
+                    );
                     Err(rgb_function_err)
                 }
             }
         }
     }
+}
+
+fn decorate_color_arg<'a>(arg: Arg<'a, 'a>) -> Arg<'a, 'a> {
+    arg.required(true)
+        .help("CSS-like color value, e.g. #00FF11 or 'rgb(255 128 0)'.")
+        .validator(is_color)
 }
 
 fn main() {
@@ -41,46 +70,38 @@ fn main() {
             Arg::with_name("v")
                 .short("v")
                 .multiple(true)
-                .help("Increase message verbosity.")
+                .help("Increase message verbosity."),
         )
         .subcommand(
             SubCommand::with_name("contrast")
                 .about("Calculate WCAG contrast of two colors.")
-                .arg(
-                    Arg::with_name("color_1")
-                        .required(true)
-                        .help(COLOR_ARG_HELP)
-                )
-                .arg(
-                    Arg::with_name("color_2")
-                        .required(true)
-                        .help(COLOR_ARG_HELP)
-                )
+                .arg(decorate_color_arg(Arg::with_name("color_1")))
+                .arg(decorate_color_arg(Arg::with_name("color_2"))),
         )
         .get_matches();
 
     let verbosity = matches.occurrences_of("v") as usize;
-    env_logger::builder().filter_level(match &verbosity {
-        0 => LevelFilter::Error,
-        1 => LevelFilter::Warn,
-        2 => LevelFilter::Info,
-        3 => LevelFilter::Debug,
-        _ => LevelFilter::Trace,
-    }).init();
+    env_logger::builder()
+        .filter_level(match &verbosity {
+            0 => LevelFilter::Error,
+            1 => LevelFilter::Warn,
+            2 => LevelFilter::Info,
+            3 => LevelFilter::Debug,
+            _ => LevelFilter::Trace,
+        })
+        .init();
 
     match matches.subcommand_matches("contrast") {
         Some(matches) => {
             let color_1_str = matches.value_of("color_1").unwrap();
             let color_2_str = matches.value_of("color_2").unwrap();
 
-            match parse_color(color_1_str) {
-                Err(_) => error!("Could not parse color 1."),
-                Ok(color_1) => match parse_color(color_2_str) {
-                    Err(_) => error!("Could not parse color 2."),
-                    Ok(color_2) => contrast::print_contrast(&color_1, &color_2, verbosity).expect("Could not print contrast.")
-                },
-            }
+            let color_1 = parse_color(color_1_str).unwrap();
+            let color_2 = parse_color(color_2_str).unwrap();
+
+            contrast::print_contrast(&color_1, &color_2, verbosity)
+                .expect("Could not print contrast.");
         }
-        None => error!("No subcommand provided. See --help.")
+        None => eprintln!("No subcommand provided. See --help."),
     }
 }
