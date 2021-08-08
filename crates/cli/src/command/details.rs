b645238983406b9679a984b42ae965463ca62d3b
FelixRilling <contact@rilling.dev>
2021-08-08 18:30:04 +0200

    Added more details to be printed.

--- crates/cli/src/command/details.rs
+++ crates/cli/src/command/details.rs
@@ -9,17 +9,39 @@ use crate::color_printing::print_color;
 use crate::options::Options;
 
 pub fn print_details(color: &Rgb, options: &Options) -> std::io::Result<()> {
-    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    let mut out = StandardStream::stdout(ColorChoice::Auto);
 
-    write!(&mut stdout, "Details for color ")?;
-    print_color(&mut stdout, color, &options.format)?;
-    writeln!(&mut stdout, ":")?;
+    write!(&mut out, "Details for color ")?;
+    print_color(&mut out, color, &options.format)?;
+    writeln!(&mut out, ":")?;
+    writeln!(&mut out, "-------")?;
 
-    write!(&mut stdout, "In RGB hexadecimal notation: ")?;
-    print_color(&mut stdout, color, &ColorFormat::RgbHex)?;
-    writeln!(&mut stdout, ".")?;
+    print_general_details(&mut out, color)?;
 
-    write!(&mut stdout, "In RGB function notation: ")?;
-    print_color(&mut stdout, color, &ColorFormat::RgbFunction)?;
-    writeln!(&mut stdout, ".")
+    print_format_details(&mut out, color)
+}
+
+fn print_general_details(out: &mut StandardStream, color: &Rgb) -> std::io::Result<()> {
+    writeln!(out, "General: ")?;
+    writeln!(out, "\tIs opaque: {}.", color.is_opaque())?;
+    writeln!(
+        out,
+        "\tEvery channel can be represented by a single byte: {}.",
+        color.channels_fit_in_u8()
+    )
+}
+
+fn print_format_details(out: &mut StandardStream, color: &Rgb) -> std::io::Result<()> {
+    writeln!(out, "Formats: ")?;
+
+    write!(out, "\tIn RGB hexadecimal notation: ")?;
+    print_color(out, color, &ColorFormat::RgbHex)?;
+    if !color.channels_fit_in_u8() {
+        write!(out, " (Warning: Channel values were rounded)")?;
+    }
+    writeln!(out, ".")?;
+
+    write!(out, "\tIn RGB function notation: ")?;
+    print_color(out, color, &ColorFormat::RgbFunction)?;
+    writeln!(out, ".")
 }
