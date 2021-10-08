5913bb194832a0b53175bb98c8728e7efc933f5f
FelixRilling <contact@rilling.dev>
2021-10-08 20:40:47 +0200

    Revert "Continued migrating to palette colors."
    This reverts commit c7b884e2089909c42ab725615860efec34dc084c.

--- crates/cli/src/color_parsing.rs
+++ crates/cli/src/color_parsing.rs
@@ -3,14 +3,14 @@ use palette::Srgba;
 
 use color_utils::error::ParsingError;
 use color_utils::model::rgb::Rgb;
-use color_utils::parser::color_as_string;
 
 pub fn parse_color(seq: &str) -> Result<Srgba, ParsingError> {
     debug!("Attempting to parse '{}'.", seq);
     let result = color_utils::parser::parse_color(seq);
 
     if let Ok(ref color) = result {
-        debug!("Parsed '{}' as '{}'.", seq, color_as_string(color));
+        let rgb: Rgb = color.to_owned().into();
+        debug!("Parsed '{}' as '{}'.", seq, rgb);
     }
 
     result
--- crates/lib/src/parser.rs
+++ crates/lib/src/parser.rs
@@ -1,4 +1,4 @@
-use cssparser::{BasicParseError, BasicParseErrorKind, Color, Parser, ParserInput, RGBA, ToCss};
+use cssparser::{BasicParseError, BasicParseErrorKind, Color, Parser, ParserInput};
 use palette::Srgba;
 
 use crate::error::ParsingError;
@@ -19,20 +19,10 @@ pub fn parse_color(seq: &str) -> Result<Srgba, ParsingError> {
     let color = Color::parse(&mut Parser::new(&mut input))?;
     match color {
         Color::CurrentColor => Err(ParsingError::InvalidSyntax(
-            "'currentcolor' is not supported in this context.",
+            "currentcolor is not supported in this context.",
         )),
         Color::RGBA(rgba) => Ok(Srgba::from_components((
-            rgba.red_f32(),
-            rgba.green_f32(),
-            rgba.blue_f32(),
-            rgba.alpha_f32(),
+            rgba.red_f32(), rgba.green_f32(), rgba.blue_f32(), rgba.alpha_f32(),
         ))),
     }
-}
-
-
-/// Creates CSS string representation for color.
-pub fn color_as_string(color: &Srgba) -> String {
-    let rgba = RGBA::from_floats(color.red, color.green, color.blue, color.alpha);
-    rgba.to_css_string()
 }
