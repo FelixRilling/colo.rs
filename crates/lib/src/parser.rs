3ae80dad1ce4e0703e29c41553bf778d1c4306f4
FelixRilling <contact@rilling.dev>
2023-08-13 10:30:25 +0200

    started updating cssparser.

--- crates/lib/src/parser.rs
+++ crates/lib/src/parser.rs
@@ -1,5 +1,6 @@
 use cssparser::{BasicParseError, BasicParseErrorKind, Color, Parser, ParserInput};
-use palette::rgb::Rgba;
+use palette::rgb::{Rgb, Rgba};
+use palette::WithAlpha;
 
 use crate::error::ParsingError;
 
@@ -19,16 +20,17 @@ impl From<BasicParseError<'_>> for ParsingError<'_> {
 /// - All other errors: See `cssparser` `Color::parse`.
 pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 	let mut input = ParserInput::new(seq);
-	let color = Color::parse(&mut Parser::new(&mut input))?;
+	let mut parser = Parser::new(&mut input);
+	let color = Color::parse(&mut parser)
+		.map_err(|_| ParsingError::InvalidSyntax("invalid syntax"))?;
+
 	match color {
 		Color::CurrentColor => Err(ParsingError::UnsupportedValue(
 			"currentcolor is not supported in this context",
 		)),
-		Color::RGBA(rgba) => Ok(Rgba::new(
-			rgba.red_f32(),
-			rgba.green_f32(),
-			rgba.blue_f32(),
-			rgba.alpha_f32(),
-		)),
+		Color::Rgba(rgba) => Ok(Rgb::new(rgba.red, rgba.green, rgba.blue)
+			.with_alpha(rgba.alpha)
+			.into_format()),
+		_ => Err(ParsingError::UnsupportedValue("format is not supported")),
 	}
 }
