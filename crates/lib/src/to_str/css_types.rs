d60328b669b1964defad7e7ba95997525873646f
FelixRilling <contact@rilling.dev>
2023-08-23 15:09:27 +0200

    Removed dependency on utils in lib.

--- crates/lib/src/to_str/css_types.rs
+++ crates/lib/src/to_str/css_types.rs
@@ -1,23 +1,20 @@
 use palette::RgbHue;
 
-use color_utils_internal::ceil_n_decimals;
-
 use crate::to_str::ChannelUnit;
 
-// 2 decimal places seems to be good enough to avoid most float-related issues and still preserve most information.
-const RELEVANT_DECIMAL_PLACES: u8 = 2;
+// used over default string formatting to only use decimal places if needed.
+fn ceil_two_decimal_places(val: f32) -> f32 {
+	(val * 100.0).ceil() / 100.0
+}
 
 /// Formats a float as a CSS number (e.g. `0.6` as `'0.6'`).
 pub(crate) fn format_number(val: f32) -> String {
-	format!("{}", ceil_n_decimals(val.into(), RELEVANT_DECIMAL_PLACES))
+	format!("{}", ceil_two_decimal_places(val))
 }
 
 /// Formats a float as a CSS percentage (e.g. `0.6` as `'60%'`).
 pub(crate) fn format_percentage(val: f32) -> String {
-	format!(
-		"{}%",
-		ceil_n_decimals((val * 100.0).into(), RELEVANT_DECIMAL_PLACES)
-	)
+	format!("{}%", ceil_two_decimal_places(val * 100.0))
 }
 
 /// Formats a float as an alpha-value.
