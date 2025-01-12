d1574549566a05e35f048587f3fb7ad8b010c34d
RillingDev <contact@rilling.dev>
2025-01-12 16:14:33 +0100

    [breaking] Always take on color as argument

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -13,6 +13,9 @@ const COLOR_ARG_HELP: &str = "CSS-like color value, e.g. #00FF11 or 'rgb(255 128
 #[command(version, about, long_about = None)]
 #[command(propagate_version = true)]
 struct Cli {
+	#[arg(required = true, help = COLOR_ARG_HELP)]
+	color: String,
+
 	#[arg(
 		long,
 		required = false,
@@ -28,17 +31,11 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-	#[command(about = "Prints the details of a color")]
-	Details {
-		#[arg(required = true, help = COLOR_ARG_HELP)]
-		color: String,
-	},
+	#[command(about = "Prints the details of the color")]
+	Details {},
 
-	#[command(about = "Calculates the WCAG contrast of two colors")]
+	#[command(about = "Calculates the WCAG contrast to another color")]
 	Contrast {
-		#[arg(required = true, help = COLOR_ARG_HELP)]
-		color: String,
-
 		#[arg(required = true, help = COLOR_ARG_HELP)]
 		other_color: String,
 	},
@@ -54,16 +51,15 @@ fn main() {
 	};
 
 	match args.command {
-		Commands::Details { color: color_str } => match color_parsing::parse_color(&color_str) {
+		Commands::Details {} => match color_parsing::parse_color(&args.color) {
 			Err(e_1) => eprintln!("Could not parse color: {}.", e_1),
 			Ok(color) => {
 				command::print_details(&color, &options).expect("Could not print details.")
 			}
 		},
 		Commands::Contrast {
-			color: color_str,
 			other_color: other_color_str,
-		} => match color_parsing::parse_color(&color_str) {
+		} => match color_parsing::parse_color(&args.color) {
 			Err(e_1) => eprintln!("Could not parse color: {}.", e_1),
 			Ok(color) => match color_parsing::parse_color(&other_color_str) {
 				Err(e_2) => eprintln!("Could not parse other color: {}.", e_2),
