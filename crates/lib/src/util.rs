23e1b9a18cbb660e2306e62c2381b49a7cd26388
RillingDev <contact@rilling.dev>
2025-01-14 16:52:50 +0100

    Removed unused helper.

--- crates/lib/src/util.rs
+++ crates/lib/src/util.rs
@@ -6,22 +6,6 @@ pub fn is_opaque(srgba: &Srgba) -> bool {
 	srgba.eq(&srgba.with_alpha(1.0))
 }
 
-/// Checks if all channels can be represented using the u8 (number from 0 to 255) format.
-pub fn channels_fit_in_u8(srgba: &Srgba) -> bool {
-	channel_fits_in_u8(srgba.red)
-		&& channel_fits_in_u8(srgba.green)
-		&& channel_fits_in_u8(srgba.blue)
-		&& channel_fits_in_u8(srgba.alpha)
-}
-
-fn channel_fits_in_u8(channel: f32) -> bool {
-	is_integer(channel * 255.0)
-}
-
-fn is_integer(val: f32) -> bool {
-	val.eq(&val.trunc())
-}
-
 #[cfg(test)]
 mod tests {
 	use palette::Srgba;
@@ -41,20 +25,5 @@ mod tests {
 		let color: Srgba = Srgba::new(1.0, 1.0, 1.0, 1.0);
 
 		assert!(is_opaque(&color));
-	}
-
-	#[test]
-	fn fits_in_u8_false_if_too_precise() {
-		let color: Srgba = Srgba::new(0.0001, 1.0, 1.0, 1.0);
-
-		assert!(!channels_fit_in_u8(&color));
-	}
-
-	#[test]
-	fn fits_in_u8_true_if_fitting() {
-		// 0.2 -> 51 in u8
-		let color: Srgba = Srgba::new(0.2, 1.0, 1.0, 1.0);
-
-		assert!(channels_fit_in_u8(&color));
 	}
 }
