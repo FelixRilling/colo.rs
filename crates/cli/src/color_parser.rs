ea74855fa73e7c35d0ff86159792b203793ff216
RillingDev <contact@rilling.dev>
2025-01-14 16:19:49 +0100

    Removed lifetimes.

--- crates/cli/src/color_parser.rs
+++ crates/cli/src/color_parser.rs
@@ -9,13 +9,13 @@ use std::option::Option::None;
 
 /// Kinds of errors than may happen during color parsing.
 #[derive(Debug)]
-pub enum ParsingError<'a> {
-	InvalidSyntax(&'a str),
+pub enum ParsingError {
+	InvalidSyntax(String),
 
-	UnsupportedValue(&'a str),
+	UnsupportedValue(String),
 }
 
-impl Display for ParsingError<'_> {
+impl Display for ParsingError {
 	fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
 		match self {
 			ParsingError::InvalidSyntax(details) => f.write_str(details),
@@ -24,7 +24,7 @@ impl Display for ParsingError<'_> {
 	}
 }
 
-impl Error for ParsingError<'_> {
+impl Error for ParsingError {
 	fn source(&self) -> Option<&(dyn Error + 'static)> {
 		match self {
 			ParsingError::InvalidSyntax(_) => None,
@@ -33,12 +33,17 @@ impl Error for ParsingError<'_> {
 	}
 }
 
-impl From<ParseError<'_, ()>> for ParsingError<'_> {
+impl From<ParseError<'_, ()>> for ParsingError {
 	fn from(err: ParseError<'_, ()>) -> Self {
-		ParsingError::InvalidSyntax(match err.kind {
-			ParseErrorKind::Basic(_) => "Invalid syntax",
-			_ => "Unknown error",
-		})
+		ParsingError::InvalidSyntax(format!(
+			"{} at L{}:{}",
+			match err.kind {
+				ParseErrorKind::Basic(kind) => kind.to_string(),
+				ParseErrorKind::Custom(_) => "Unknown error".to_string(),
+			},
+			err.location.line,
+			err.location.column
+		))
 	}
 }
 
@@ -52,12 +57,18 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 	let color = Color::parse(&mut Parser::new(&mut input))?;
 
 	match color {
-		Color::CurrentColor => Err(ParsingError::UnsupportedValue(
-			"currentcolor is not supported in this context",
+		Color::ColorFunction(_) => Err(ParsingError::UnsupportedValue(
+			"Format is not supported.".to_string(),
 		)),
+
+		Color::CurrentColor => Err(ParsingError::UnsupportedValue(
+			"currentcolor is not supported in this context.".to_string(),
+		)),
+
 		Color::Rgba(rgba) => Ok(Rgb::new(rgba.red, rgba.green, rgba.blue)
 			.with_alpha(rgba.alpha)
 			.into_format()),
+
 		Color::Hsl(hsl) => Ok(Hsl::new(
 			hsl.hue.unwrap_or(0.0),
 			hsl.saturation.unwrap_or(0.0),
@@ -65,6 +76,7 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 		)
 		.with_alpha(hsl.alpha.unwrap_or(1.0))
 		.into_color()),
+
 		Color::Hwb(hwb) => Ok(Hwb::new(
 			hwb.hue.unwrap_or(0.0),
 			hwb.whiteness.unwrap_or(0.0),
@@ -72,6 +84,7 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 		)
 		.with_alpha(hwb.alpha.unwrap_or(1.0))
 		.into_color()),
+
 		Color::Lab(lab) => Ok(Lab::new(
 			lab.lightness.unwrap_or(0.0),
 			lab.a.unwrap_or(0.0),
@@ -79,6 +92,7 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 		)
 		.with_alpha(lab.alpha.unwrap_or(1.0))
 		.into_color()),
+
 		Color::Lch(lch) => Ok(Lch::new(
 			lch.lightness.unwrap_or(0.0),
 			lch.chroma.unwrap_or(0.0),
@@ -86,6 +100,7 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 		)
 		.with_alpha(lch.alpha.unwrap_or(1.0))
 		.into_color()),
+
 		Color::Oklab(oklab) => Ok(Oklab::new(
 			oklab.lightness.unwrap_or(0.0),
 			oklab.a.unwrap_or(0.0),
@@ -93,6 +108,7 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 		)
 		.with_alpha(oklab.alpha.unwrap_or(1.0))
 		.into_color()),
+
 		Color::Oklch(oklch) => Ok(Oklch::new(
 			oklch.lightness.unwrap_or(0.0),
 			oklch.chroma.unwrap_or(0.0),
@@ -100,6 +116,5 @@ pub fn parse_color(seq: &str) -> Result<Rgba, ParsingError> {
 		)
 		.with_alpha(oklch.alpha.unwrap_or(1.0))
 		.into_color()),
-		Color::ColorFunction(_) => Err(ParsingError::UnsupportedValue("format is not supported")),
 	}
 }
